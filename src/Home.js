490bd9c5e0b8499562bcb016f73cec0e1bec4b36
Syafiq Hamidun <syafiqhamidun10@gmail.com>
2023-12-10 22:08:21 +0800

    props

--- src/Home.js
+++ src/Home.js
@@ -7,13 +7,15 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
- 
+
+    
+
     return ( 
         <div className="home">
             {blogs.map((book) => (
                 <div className="blog-preview" key={book.id}>
                     <h2>{book.title}</h2>
-                    <p>written by {book.title}</p>
+                    <p> written by {book.author}</p>
                 </div>
             ))}
         </div>
