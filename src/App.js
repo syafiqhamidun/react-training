6fa80f69606348a3159e09bc5a76bc7092912f9c
Syafiq Hamidun <syafiqhamidun10@gmail.com>
2023-12-10 16:43:39 +0800

    outputting list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <div className="content">
-        <p>app compontent</p>
+        <Home/>
       </div>
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
+ 
+    return ( 
+        <div className="home">
+            {blogs.map((book) => (
+                <div className="blog-preview" key={book.id}>
+                    <h2>{book.title}</h2>
+                    <p>written by {book.title}</p>
+                </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default Home;
